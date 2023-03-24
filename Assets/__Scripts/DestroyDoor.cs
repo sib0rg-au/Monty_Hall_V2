ec928bcf4698e9323b1a15b876b854ca119a6a8d
sib0rg-au <127904273+sib0rg-au@users.noreply.github.com>
2023-03-24 10:49:52 +1030

    Added animal assets and instantiate sheep when door opens

--- Assets/__Scripts/DestroyDoor.cs
+++ Assets/__Scripts/DestroyDoor.cs
@@ -10,8 +10,17 @@ public class DestroyDoor : MonoBehaviour
     public GameObject buttonDestroy1;
     public GameObject buttonDestroy2;
     public GameObject buttonDestroy3;
+    public GameObject sheep;
+    public GameObject spawnPoint1;
+    public GameObject spawnPoint2;
+    public GameObject spawnPoint3;
+
     public MontyHallGame doorInt, timerCheck;
     public bool openDoor1 = false;
+
+    public bool spawn1 = false;
+    public bool spawn2 = false;
+    public bool spawn3 = false;
 
     private float timer = 1;
     // Update is called once per frame
@@ -29,8 +38,15 @@ public class DestroyDoor : MonoBehaviour
             {
                 timer -= Time.deltaTime;
                 doorDestroy1.GetComponent<Animation>().Play();
+               
             }
 
+            while(spawn1 == false)
+            {
+                Instantiate(sheep, spawnPoint1.transform);
+                spawn1 = true;
+            }
+            
             
             Destroy(buttonDestroy1);
             //openDoor1 = true;
@@ -45,7 +61,14 @@ public class DestroyDoor : MonoBehaviour
                 timer -= Time.deltaTime;
                 doorDestroy2.GetComponent<Animation>().Play();
             }
-            
+
+            while (spawn2 == false)
+            {
+                Instantiate(sheep, spawnPoint2.transform);
+                spawn2 = true;
+            }
+
+
             Destroy(buttonDestroy2);
             //Destroy(doorDestroy2);
         }
@@ -56,7 +79,13 @@ public class DestroyDoor : MonoBehaviour
                 timer -= Time.deltaTime;
                 doorDestroy3.GetComponent<Animation>().Play();
             }
-            
+
+            while (spawn3 == false)
+            {
+                Instantiate(sheep, spawnPoint3.transform);
+                spawn3 = true;
+            }
+
             Destroy(buttonDestroy3);
             //Destroy(doorDestroy3);
         }
